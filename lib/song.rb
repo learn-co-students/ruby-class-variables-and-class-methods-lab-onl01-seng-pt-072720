33190186e7a7e703d24abb4fcccdc3bb02e2776b
Andrew Echeverria <andrew.a.echeverria@gmail.com>
2020-08-26 04:56:41 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -24,11 +24,35 @@ def self.count
   @@count
 end
 
-def genres
-  @@genres
+def self.genres
+  @@genres.uniq
 end
 
-def artists
-  @@artists
+def self.artists
+  @@artists.uniq
+end
+
+def self.genre_count
+  counted_genres = {}
+    @@genres.each do |genre|
+    if counted_genres[genre]
+      counted_genres[genre] += 1
+    else
+      counted_genres[genre] = 1
+end
+end
+counted_genres
+end
+
+def self.artist_count
+  counted_artists = {}
+  @@artists.each do |artist|
+    if counted_artists[artist]
+      counted_artists[artist] += 1
+    else
+      counted_artists[artist] = 1
+    end
+  end
+  counted_artists
 end
 end
